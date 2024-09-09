82c555bc4611ad5b77ee0d19fbab83946772e6ee
David22221 <david.delgado2101@alumnos.ubiobio.cl>
2024-09-09 00:50:57 -0300

    ADD Ejercicio variables y tipos de datos

--- Variables.js
+++ Variables.js
@@ -1,3 +1,9 @@
-const apellido = 'Salazar';
-console.log(apellido); // Salazar
-console.log(typeof apellido); // String
+const nombre = 'David';
+const edad = 21;
+const puedoConducir = false;
+console.log(nombre); 
+console.log(typeof nombre); 
+console.log(edad);
+console.log(typeof edad);
+console.log(puedoConducir);
+console.log(typeof puedoConducir);
