dd98ecab1c093d13cf5fc7e45e65c5d66f8c0d8e
David22221 <david.delgado2101@alumnos.ubiobio.cl>
2024-09-09 01:33:59 -0300

    ADD Ejercicios operadores aritmeticos

---
+++ Operadores.js
@@ -0,0 +1,33 @@
+const numero1 = 20;
+const numero2 = 40;
+
+function sumar (a,b){
+    return a + b
+
+}
+
+function resta (a,b){
+    return a - b
+
+}
+
+function multiplicacion (a,b){
+    return a * b
+
+}
+
+function division (a,b){
+    return a / b
+
+}
+
+function modulo (a,b){
+    return a % b
+
+}
+
+console.log("La suma es:", sumar(numero1,numero2))
+console.log("La resta es:", resta(numero1,numero2))
+console.log("la multiplicacion es:", multiplicacion(numero1,numero2))
+console.log("la division es:", division(numero1,numero2))
+console.log("El modulo es:", modulo(numero2,numero1))
