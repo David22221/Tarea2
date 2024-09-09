a9fbd44d30107fc14ab2b93c141cc46f30620765
David22221 <david.delgado2101@alumnos.ubiobio.cl>
2024-09-09 01:33:15 -0300

    ADD Ejercicios operadores de asignacion y comparacion

---
+++ Operadores_de_asignacion.js
@@ -0,0 +1,10 @@
+const numero1 = 15;
+const numero2 = 20;
+const numero3 = '25';
+
+console.log(`El numero ${numero1} es mayor o igual al numero ${numero2}`, numero1>=numero2)
+console.log(`El numero ${numero1} es menor o igual al numero ${numero2}`, numero1<=numero2)
+console.log(`El numero ${numero1} es menor que el numero ${numero3}`,numero1<numero3 )
+console.log(`El numero ${numero3} es menor que el numero ${numero2}`,numero3<numero2)
+console.log(`El numero ${numero3} es estrictamente diferente que el numero ${numero1}`, numero3!==numero1)
+console.log(`El numero ${numero1} es estrictamente igual al numero ${numero2}`, numero1 === numero2)
