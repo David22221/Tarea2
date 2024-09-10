694b8f7e8baff5586f4f3c13e04eb70ec919ddab
David22221 <david.delgado2101@alumnos.ubiobio.cl>
2024-09-10 13:31:19 -0300

    ADD Ejercicios objetos

---
+++ Objetos.js
@@ -0,0 +1,34 @@
+const persona = {
+    nombre: "David Delgado",
+    edad: 21,
+    genero: "Masculino"
+
+}
+console.log(persona)
+
+const caja = {
+    cuadernos: 10,
+    lapices: 5,
+    papel: 100,
+    fotografias: 15,
+    buenEstado: true
+  };
+
+console.log(caja)
+
+console.log(caja.cuadernos); 
+console.log(typeof caja.cuadernos);
+
+console.log(caja.lapices); 
+console.log(typeof caja.lapices); 
+
+console.log(caja.papel); 
+console.log(typeof caja.papel); 
+
+console.log(caja.fotografias); 
+console.log(typeof caja.fotografias); 
+
+console.log(caja.buenEstado);
+console.log(typeof caja.buenEstado); 
+
+console.log(typeof caja); 
