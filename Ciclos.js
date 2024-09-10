90a2b6e26ae4acb9dcf06675b2369c32529c30c8
David22221 <david.delgado2101@alumnos.ubiobio.cl>
2024-09-10 12:22:11 -0300

    ADD Ejercicios de ciclos

---
+++ Ciclos.js
@@ -0,0 +1,54 @@
+console.log("Numeros del 1 al 10")
+for (let i=1;i <= 10;i++) {
+    console.log(i)
+  }
+console.log("Numeros del 10 al 1")
+ for(let i=10;i>=1;i-- ){
+    console.log(i)
+ } 
+console.log("Numeros pares del 1 al 10")
+for (let i=1; i <= 10; i++){
+    if(i%2 === 0){
+        console.log(i)
+             
+    }
+}
+console.log("Numeros impares del 1 al 10")
+for (let i=1; i <= 10; i++){
+    if(i% 2 !== 0){
+        console.log(i)
+             
+    }
+}
+
+console.log("Numeros multiplos de 3 del 1 al 10")
+for (let i=1; i <= 10; i++){
+    if(i%3 === 0){
+        console.log(i)
+             
+    }
+}
+
+console.log("Numeros multiplos de 5 del 1 al 10")
+for (let i=1; i <= 10; i++){
+    if(i% 5 === 0){
+        console.log(i)
+             
+    }
+}
+
+console.log("Numeros multiplos de 3 y 5 del 1 al 10")
+for (let i=1; i <= 10; i++){
+    if(i% 3 === 0 && i%5 === 0){
+        console.log(i)
+             
+    }
+}
+
+console.log("Numeros multiplos de 3 o 5 del 1 al 10")
+for (let i=1; i <= 10; i++){
+    if(i% 3 === 0 || i%5 === 0){
+        console.log(i)
+             
+    }
+}
